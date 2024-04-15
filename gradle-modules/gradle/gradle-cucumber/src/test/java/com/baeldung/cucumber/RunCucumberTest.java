72756c17ed5cfc47ded53190ad59348ba5b88140
Bob Makowski <tritonpi-git0@yahoo.com>
2024-04-15 11:44:22 -0500

    gradle-cucumber : cleanup commented code

--- gradle-modules/gradle/gradle-cucumber/src/test/java/com/baeldung/cucumber/RunCucumberTest.java
+++ gradle-modules/gradle/gradle-cucumber/src/test/java/com/baeldung/cucumber/RunCucumberTest.java
@@ -1,15 +1,9 @@
 package com.baeldung.cucumber;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-//@CucumberOptions(
-//    plugin = {"pretty", "html:out/cucumber/cucumber-report.html"},
-//    glue = {"com.baeldung.cucumber"},
-//    features = {"src/test/resources"}
-//)
 public class RunCucumberTest {
 
 }
